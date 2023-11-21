a8850dd6d535a916ca95e9eee45dd419c664accd
Albin Varghese <albin8285@gmail.com>
2023-11-21 14:55:52 +0530

    feat: modify schema and add controller logic for storing auth logs

--- app/Http/Controllers/AuthLogController.php
+++ app/Http/Controllers/AuthLogController.php
@@ -3,64 +3,37 @@
 namespace App\Http\Controllers;
 
 use App\Models\AuthLog;
-use App\Http\Requests\StoreAuthLogRequest;
-use App\Http\Requests\UpdateAuthLogRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthLogController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreAuthLogRequest $request)
+    public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'status' => 'required|boolean',
+            'type' => 'required|string|in:face,rfid,password',
+            'message' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        try {
+            $authLog = new AuthLog();
+            $authLog->status = $request->input('status');
+            $authLog->type = $request->input('type');
+            $authLog->message = $request->input('message');
+            $authLog->save();
+
+            return response()->json(['message' => 'Auth log created successfully'], 201);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Failed to create auth log'], 500);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(AuthLog $authLog)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(AuthLog $authLog)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateAuthLogRequest $request, AuthLog $authLog)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(AuthLog $authLog)
-    {
-        //
-    }
 }
--- database/migrations/2023_11_21_070108_create_auth_logs_table.php
+++ database/migrations/2023_11_21_070108_create_auth_logs_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('auth_logs', function (Blueprint $table) {
             $table->id();
+            $table->boolean('status');
             $table->string('type');
             $table->string('message');
             $table->timestamps();
